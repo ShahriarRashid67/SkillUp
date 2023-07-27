56fe1886c1394e90f31fdeb1a15c07316a516f14
ShahriarRashid67 <mdshahriar.rashid67@gmail.com>
2023-07-27 17:08:05 +0600

    Second

--- client/src/components/pages/homePage/HomePage.jsx
+++ client/src/components/pages/homePage/HomePage.jsx
@@ -3,19 +3,23 @@ import Business from "./Business";
 import Classes from "./Classes";
 import Contact from "./Contact";
 import Instructor from "./Instructor";
+import InstructorDetails from "./InstructorDetails";
 import Pricing from "./Pricing";
 import Review from "./Review";
 import Showcase from "./Showcase";
+import StudentSteps from "./StudentSteps";
 
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
       <Showcase></Showcase>
-      <Classes></Classes>
-      <Pricing></Pricing>
+      {/* <Classes></Classes> */}
+      {/* <Pricing></Pricing> */}
       <Business></Business>
+      <StudentSteps></StudentSteps>
       <Instructor></Instructor>
+      <InstructorDetails></InstructorDetails>
       <Review></Review>
       <Contact></Contact>
     </div>
